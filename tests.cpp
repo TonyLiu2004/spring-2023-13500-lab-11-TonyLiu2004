4ad127ac629dfd7cd176f3c8156e3ca597e9fb2f
TonyLiu <ltsplice@gmail.com>
2023-04-18 13:40:09 -0400

    added tests for task 2

--- tests.cpp
+++ tests.cpp
@@ -2,6 +2,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "profile.h"
+#include "network.h"
 
 TEST_CASE("Profile Class"){
     Profile p1("marco", "Marco");
@@ -15,4 +16,19 @@ TEST_CASE("Profile Class"){
     Profile p2("tarma1", "Tarma Roving");
     CHECK(p2.getUsername() == "tarma1");
     CHECK(p2.getFullName() == "Tarma Roving (@tarma1)");
+}
+
+TEST_CASE("Network Class"){
+    Network nw;
+    CHECK(nw.addUser("mario", "Mario") == true);
+    CHECK(nw.addUser("luigi", "Luigi") == true);
+
+    CHECK(nw.addUser("mario", "Mario2") == false);
+    CHECK(nw.addUser("mario 2", "Mario2") == false);
+    CHECK(nw.addUser("mario-2", "Mario2") == false);
+
+    for(int i = 2; i < 20; i++)
+    CHECK(nw.addUser("mario" + std::to_string(i),"Mario" + std::to_string(i)) == true);
+
+    CHECK(nw.addUser("yoshi", "Yoshi") == false);
 }
